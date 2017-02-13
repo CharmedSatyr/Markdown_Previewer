0651d75c770bc5d681b6c1a146ce5cda8efedccc
CharmedSatyr <z@charmed.tech>
2017-02-13 09:01:38 +0900

    Updated styling and boilerplate text.

--- build/transformed.js
+++ build/transformed.js
@@ -9460,7 +9460,7 @@ var App = React.createClass({
 	displayName: 'App',
 
 	getInitialState: function () {
-		return { userInput: '# Markdown language\n## is easy!\n### Headings take one to six hashes\n\n---\n\nUnordered list:\n* Two *ways* to make _italics_.\n* **Bold** is __similar__.\n*   Combine the two **_easily_**.\n\n***\n\nNumbered list:\n1. `Monospace`\n 1. An extra space precedes this sub-item.\n2. ~~Strikethrough~~\n * This one, too!\n---\n\n>Blockquotes\n\n___\n\n[Find out more here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet \"Optional Title\")\n==' };
+		return { userInput: '# Markdown language\n## is easy!\n### Headings take one to six hashes\n\n---\n\nUnordered list:\n* There are two *ways* to make _italics_.\n* **Bold** is __similar__.\n*   Combine the two **_easily_**.\n\n***\n\nNumbered list:\n1. `Monospace`\n 1. ←An extra space precedes this sub-item.\n2. ~~Strikethrough~~\n * ←There\'s an extra space before this one, too!\n---\n\n>Blockquotes are made like so.\n\n___\n\n[Find out more here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet \"Optional Title\")\n--' };
 	},
 	handleUserInput: function (e) {
 		this.setState({
